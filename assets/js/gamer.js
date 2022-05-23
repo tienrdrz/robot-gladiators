0888d7fd3dfe133174805fe19dbffb0a603a32a0
etienne <etiennerodriguez3@gmail.com>
2022-05-23 13:25:13 -0400

    added feature

--- assets/js/gamer.js
+++ assets/js/gamer.js
@@ -33,12 +33,13 @@ var fightOrSkip = function() {
 }
 
 var fight = function(enemy) {
-    while (playerInfo.health > 0 && enemy.health > 0) {
-  if (fightOrSkip()); {
-    //if true leave fight 
-    break;
-  }
-      // remove enemy's health by subtracting the amount set in the playerInfo.attack variable
+  while (playerInfo.health > 0 && enemy.health > 0) {
+    // ask player if they'd like to fight or skip using fightOrSkip function
+    if (fightOrSkip()) {
+      // if true, leave fight by breaking loop
+      break;
+    }
+  // remove enemy's health by subtracting the amount set in the playerInfo.attack variable
       var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
 
       enemy.health = Math.max(0, enemy.health - damage);
@@ -177,22 +178,19 @@ else {
 
   var shop = function() {
     var shopOptionPrompt = window.prompt(
-      "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+      "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
     );
-
+    shopOptionPrompt = parseInt(shopOptionPrompt);
     switch (shopOptionPrompt) {
-      case "REFILL": // new case
-      case "refill":
+      case 1:
         playerInfo.refillHealth();
     
         break;
-      case "UPGRADE": // new case
-      case "upgrade":
+      case 2:
         playerInfo.upgradeAttack();
         
         break;
-      case "LEAVE": // new case
-      case "leave":
+      case 3:
         window.alert("Leaving the store.");
         break;
       default:
