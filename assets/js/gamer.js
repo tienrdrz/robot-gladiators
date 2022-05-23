1bcda44c3c98007764a3c93d9e6cc211fdf6f9a8
etienne <etiennerodriguez3@gmail.com>
2022-05-23 11:29:26 -0400

    a

--- assets/js/gamer.js
+++ assets/js/gamer.js
@@ -117,42 +117,42 @@ else {
       "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
     );
 
-switch (shopOptionPrompt) {
-case "REFILL":
-case "refill":
-  if (playerMoney >= 7) {
-  window.alert("Refilling player's health by 20 for 7 dollars.");
-
-  playerHealth = playerHealth + 20;
-  playerMoney = playerMoney - 7;
-}
-else {
-  window.alert("You don't have enough money!");
-}
-  break;
-case "UPGRADE":
-case "upgrade":
-  if (playerMoney >= 7) {
-  window.alert("Upgrading player's attack by 6 for 7 dollars.");
-
-  playerAttack = playerAttack + 6;
-  playerMoney = playerMoney - 7;
-}
-else {
-  window.alert("You don't have enough money!");
-}
-  break;
-
-case "LEAVE":
-case "leave": 
-window.alert("Leaving the store")
-
-default: 
-window.alert("You did not pick a valid option. Try again");
-
-shop();
-break;
-}    
+    switch (shopOptionPrompt) {
+      case "REFILL": // new case
+      case "refill":
+        if (playerMoney >= 7) {
+          window.alert("Refilling player's health by 20 for 7 dollars.");
+    
+          playerHealth = playerHealth + 20;
+          playerMoney = playerMoney - 7;
+        }
+        else {
+          window.alert("You don't have enough money!");
+        }
+    
+        break;
+      case "UPGRADE": // new case
+      case "upgrade":
+        if (playerMoney >= 7) {
+          window.alert("Upgrading player's attack by 6 for 7 dollars.");
+    
+          playerAttack = playerAttack + 6;
+          playerMoney = playerMoney - 7;
+        }
+        else {
+          window.alert("You don't have enough money!");
+        }
+    
+        break;
+      case "LEAVE": // new case
+      case "leave":
+        window.alert("Leaving the store.");
+        break;
+      default:
+        window.alert("You did not pick a valid option. Try again.");
+        shop();
+        break;
+    }  
   };
 
  startGame();
